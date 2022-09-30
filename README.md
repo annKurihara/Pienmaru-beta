9e05bf8b8cec535ef52ba7727a651d33413e7aec
annKurihara <94680659+annKurihara@users.noreply.github.com>
2022-09-30 18:22:38 +0900

    added music play function

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pienmaru-bot-14
+# pienmaru-kai
 Pienmaru source code compatible for discord.js v.14
+
+need to install:
+npm i ffmpeg-static
+npm i opusscript
+npm i libsodium-wrappers
--- index.js
+++ index.js
@@ -1,22 +1,71 @@
-const { token } = require('./config.json');
-const { Client, IntentsBitField, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { prefix, token } = require('./config.json');
+const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
+const { DisTube } = require("distube");
 
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds, 
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-] });
+const client = new Client({
+    partials: [
+      Partials.Channel, // for text channel
+      Partials.GuildMember, // for guild member
+      Partials.User, // for discord user
+    ],
+    intents: [
+      GatewayIntentBits.Guilds, // for guild related things
+      GatewayIntentBits.GuildMembers, // for guild members related things
+      GatewayIntentBits.GuildIntegrations, // for discord Integrations
+      GatewayIntentBits.GuildVoiceStates, // for voice related things
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent
+    ],
+});
+
+const distube = new DisTube(client, {
+	searchSongs: 5,
+	searchCooldown: 30,
+	leaveOnEmpty: false,
+	leaveOnFinish: false,
+	leaveOnStop: false,
+});
+
+
 
 client.once('ready', () => onClientReady());
 function onClientReady() {
-    console.log("Pienmaru β is ready!");
+    console.log("Pienmaru is ready!");
     client.on('messageCreate', (message) => onMessageReceived(message));
 }
 
 function onMessageReceived(message) {
-    if (message.content.toLowerCase() === "hewwo") {
-        message.reply("em bek OwO");
+    
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
+    const command = args.shift();
+
+    if (message.content.toLowerCase() === 'hello') {
+        message.reply("HEWWOOOOO OwO");
+    } 
+    else if (message.content.startsWith(prefix) && (command==='play'|| command==='p')) {
+        distube
+            .play(message.member.voice.channel, args.join(' '), {
+                message,
+                textChannel: message.channel,
+                member: message.member,
+            })
+            .catch(err => {
+                message.reply(err.message);
+            });
     }
 }
+
+// Send "Now Playing" song status
+    distube
+	.on('playSong', (queue, song) =>
+		queue.textChannel?.send(
+			`Playing \`${song.name}\` - \`${
+				song.formattedDuration
+			}\`\nRequested by: ${song.user}`,
+		),
+	)
 
 client.login(token);
